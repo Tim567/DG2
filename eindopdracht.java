3fb106fb8e804e42c6d623e8a65003f18f351f38
Tim Bentum <tim_bentum@live.nl>
2019-05-21 10:56:58 +0200

    Random woorden gemaakt

--- eindopdracht.java
+++ eindopdracht.java
@@ -1,7 +1,10 @@
-package DG2;
 
 public class eindopdracht {
     public static void main(String[] args){
+        String[] woorden = {"op","bliksemafleider","ontbrekende","problematische","speciaal","paden","vluchtroutes","vanuit","verbindingen","ombouwde","mythen","studentes","toelichting","maatstaf","scheidslijn","democratische","ritselen","reikt","verklaarbaar","esthetisch","bijvoeglijk","begrenzing","hoe","debet","indicatoren","westerns","haast","knelpunten","klik","nam"};
+        for(int x = 0; x < woorden.length; x++){
+            System.out.println(woorden[x]);
+        }
 
     }
 }
