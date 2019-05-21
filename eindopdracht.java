c859deae65050acfd5520c17e386fa8ee5f020c8
JordiTJ <jordikay@hotmail.nl>
2019-05-21 11:14:08 +0200

    Added comment line

--- eindopdracht.java
+++ eindopdracht.java
@@ -2,6 +2,7 @@ package DG2;
 
 public class eindopdracht {
     public static void main(String[] args){
+        //Tim Bentum
         String[] woorden = {"op","bliksemafleider","ontbrekende","problematische","speciaal","paden","vluchtroutes","vanuit","verbindingen","ombouwde","mythen","studentes","toelichting","maatstaf","scheidslijn","democratische","ritselen","reikt","verklaarbaar","esthetisch","bijvoeglijk","begrenzing","hoe","debet","indicatoren","westerns","haast","knelpunten","klik","nam"};
         for(int x = 0; x < woorden.length; x++){
             System.out.println(woorden[x]);
